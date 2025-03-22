f6634c3871b3587a8f05f8d11318a50c4f2f8b05
JoaoMathus <joaomatheusgdo@gmail.com>
2025-03-21 22:02:03 -0300

    Reserva refatorada para seu arquivo header

---
+++ reserva.h
@@ -0,0 +1,32 @@
+/*
+ * arquivo: reserva.h
+ * resumo: contém o tipo reserva_t e as enums prioridade_t
+ *         e tipo_quarto_t.
+ */
+
+#ifndef RESERVA_H
+#define RESERVA_H
+
+#include "data.h" /* para data_t */
+
+typedef struct reserva {
+    int id;
+    char nome_hospede[64];
+    data_t data_checkin;
+    prioridade_t prioridade;
+    tipo_quarto_t tipo_quarto;
+} reserva_t;
+
+typedef enum prioridade {
+    VIP = 1,
+    PADRAO,
+    ECONOMICO
+} prioridade_t;
+
+typedef enum tipo_quarto {
+    SINGLE = 0,
+    DUPLO,
+    SUITE
+} tipo_quarto_t;
+
+#endif /* RESERVA_H */
